1012a775868fc3849c4e94f0825b8329b1f041f2
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-14 20:29:10 -0400

    returning value

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+//Create two variables: One that holds a fictional user input (a number of your choice) and one that holds no value initially (e.g. "result"). 
+let fictional_input = 5
+//Set the result variable to 18 plus the value stored in user input. 
+let result = 18 + fictional_input;
+//  Add three additional lines of code where you change the result variable again (by subtracting a value of your choice, multiplying it and dividing it). 
+result = result - 2
+result = result * 3
+result = result / 6
+alert(result);
