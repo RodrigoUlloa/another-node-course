58bfd0b0d94ad63dbd93c822337e513551fae83f
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-23 14:37:38 -0400

    using if, checking the win condition

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 10;
 
-let  chosenMaxLife = 100;
+let chosenMaxLife = 100;
 let currentMonterHealt = chosenMaxLife;
 let currentPlayerHealt = chosenMaxLife;
 
@@ -9,6 +10,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonterHealt -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealt -= playerDamage;
+  if (currentMonterHealt <= 0 && currentPlayerHealt > 0){
+    alert('c muere');
+  } else if (currentPlayerHealt <= 0 && currentMonterHealt > 0) {
+    alert('me muero unu')
+  } else if (currentPlayerHealt <= 0 && currentMonterHealt <= 0) {
+    alert('empate')
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
