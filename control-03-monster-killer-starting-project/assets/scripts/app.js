0d691176e57cf1b16fe7099903befae531a22ad0
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-23 15:22:47 -0400

    heal functionality

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
-const MONSTER_ATTACK_VALUE = 14;
+const MONSTER_ATTACK_VALUE = 16;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonterHealt = chosenMaxLife;
@@ -8,15 +9,7 @@ let currentPlayerHealt = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode) {
-  let maxDamage;
-  if (mode === 'ATTACK'){
-    maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-  const damage = dealMonsterDamage(maxDamage);
-  currentMonterHealt -= damage;
+function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealt -= playerDamage;
   if (currentMonterHealt <= 0 && currentPlayerHealt > 0){
@@ -28,6 +21,18 @@ function attackMonster(mode) {
   }
 }
 
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK'){
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonterHealt -= damage;
+  endRound();
+}
+
 function attackHandler()  {
   attackMonster('ATTACK');
 }
@@ -36,5 +41,19 @@ function strongAstrongAttackBtn(){
   attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler(){
+  let healValue;
+  if (currentPlayerHealt >= chosenMaxLife - HEAL_VALUE){
+    alert('ya teni mucha vida deja de wear');
+    healValue = chosenMaxLife - currentPlayerHealt;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(healValue);
+  currentPlayerHealt += healValue;
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAstrongAttackBtn);
+healBtn.addEventListener('click', healPlayerHandler)
