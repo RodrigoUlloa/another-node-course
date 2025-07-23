a4cd871fede70b29d5fcae8a20e609fde07929d4
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-23 14:48:34 -0400

    adding strong attack, refacor for DRY

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
-const MONSTER_ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonterHealt = chosenMaxLife;
@@ -7,8 +8,14 @@ let currentPlayerHealt = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK'){
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonterHealt -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealt -= playerDamage;
@@ -21,4 +28,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler()  {
+  attackMonster('ATTACK');
+}
+
+function strongAstrongAttackBtn(){
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAstrongAttackBtn);
