640253f4eb9385a7d6bbb7ad531315033ec38bb4
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-30 18:19:11 -0400

    try-catch & handle errors

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -11,14 +11,26 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt('cuanta vida queri', '100');
-
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 let lastLoggedEntry;
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
-  chosenMaxLife = 100;
+function getMaxLifeValues(){
+  const enteredValue = prompt('cuanta vida queri', '100');
+
+  const parsedValue = parseInt(enteredValue);
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw { message: 'Invalid user input, not a number' };
+  }
+  return parsedValue;
+}
+let chosenMaxLife
+try{
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
+  // chosenMaxLife = 100;
+  // alert('you entered something wrong, defualt value of 100 was used');
+  throw error;
 }
 
 let currentMonterHealth = chosenMaxLife;
