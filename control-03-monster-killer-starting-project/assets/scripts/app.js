ae9cad47d06da6549b5398368d5ac0f12c504805
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-29 20:57:11 -0400

    for of loop

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -222,7 +222,23 @@ function healPlayerHandler(){
 }
 
 function printLogHandler(){
-  console.log(battleLog);
+  for (let i = 0; i < 3; i++){
+    console.log('--------------');
+  }
+
+  // for (let i = 10; i > 0;){
+  //   i--;
+  //   console.log(i);
+  // }
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i]);
+  // }
+  let i = 0;
+  for (const logEntry of battleLog){
+    console.log(logEntry);
+    console.log(i);
+    i++;
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
