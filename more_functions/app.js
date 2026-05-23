80af78ab7b791bc8ebea8279f19a6675409ba268
jarv <rodrigo.ulloa22@gmail.com>
2026-05-23 17:46:18 -0400

    sections realize 123 - 129

---
+++ more_functions/app.js
@@ -0,0 +1,17 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const start = function () {
+  console.log('Game is starting...');
+};
+
+// const person = {
+//   greet: function greet() {
+//     console.log("Hello there!")
+//   }
+// };
+
+// person.greet();
+
+
+
+startGameBtn.addEventListener('click', start);
