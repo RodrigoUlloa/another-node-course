4a112ea32bb1b31c7be76ffbcbdb5dbe10b28bc1
jarv <rodrigo.ulloa22@gmail.com>
2026-05-23 22:37:56 -0400

    lesson 132 implementing the core game logic

--- more_functions/app.js
+++ more_functions/app.js
@@ -2,18 +2,49 @@ const startGameBtn = document.getElementById("start-game-btn");
 
 const ROCK = "ROCK";
 const PAPER = "PAPER";
-const SCISSORS = "SCISSORSS";
+const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gameIsRunning = false;
 
 const getPlayerChoice = function () {
-  const selection = prompt(`${ROCK}, ${PAPER} o ${SCISSORS}`, "").toUpperCase();
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    "",
+  ).toUpperCase();
   if (selection !== ROCK && selection != PAPER && selection != SCISSORS) {
     alert(`Invalid choice! We chose  Rock for You`);
     return DEFAULT_USER_CHOICE;
   }
   return selection;
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
 };
 
 startGameBtn.addEventListener("click", function () {
@@ -22,6 +53,8 @@ startGameBtn.addEventListener("click", function () {
   }
   gameIsRunning = true;
   console.log("Game is stariting...");
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
