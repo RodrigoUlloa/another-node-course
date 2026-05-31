c900fba263354622b65fa0135541f87aaaede933
jarv <rodrigo.ulloa22@gmail.com>
2026-05-31 08:23:15 -0400

    lesson 151 - 154 + assigment 5 dom querying

---
+++ working_with_DOM/dom-assignment-start/assignment.js
@@ -0,0 +1,16 @@
+const task1 = document.getElementById("task-1");
+task1.style.color = "white";
+task1.style.backgroundColor = "black";
+
+const task1v2 = document.querySelector("#task-1");
+task1v2.style.color = "white";
+task1v2.style.backgroundColor = "black";
+
+const task2 = document.querySelector("title");
+task2.textContent = "Assignment - Solved!";
+
+const task2v2 = document.head.querySelector("title");
+task2v2.textContent = "Assignment - Solved!";
+
+const task3 = document.querySelector("h1");
+task3[0].textContent = "Assignment - Solved!";
