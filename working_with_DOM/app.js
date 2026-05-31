e3c85908e1e5adc8d9b8a195f926a808800484ee
jarv <rodrigo.ulloa22@gmail.com>
2026-05-31 14:15:42 -0400

    lesson 159 DOM traversal vs Query methods

--- working_with_DOM/app.js
+++ working_with_DOM/app.js
@@ -1,22 +1,3 @@
-const h1 = document.getElementById("main-title");
-
-h1.textContent = "Some new title";
-h1.style.color = "white";
-h1.style.backgroundColor = "black";
-
-const li = document.querySelector("li:last-of-type");
-li.textContent = li.textContent + " (changed!)";
-
-const body = document.body;
-
-//const listItemElements = document.querySelectorAll("li");
-const listItemElements = document.getElementsByTagName("li");
-
-for (const listItemEl of listItemElements) {
-  console.dir(listItemEl);
-}
-console.log(firstLi);
-
 const ul = document.body.firstElementChild.nextElementSibling;
 const firstLi = ul.firstElementChild;
 
