e38b4a91cb13cc883f28535f718c6cbbb6c55005
jarv <rodrigo.ulloa22@gmail.com>
2026-06-01 00:00:29 -0400

    lesson 172 opening a modal with css clases

--- working_with_DOM/dom-06-demo-project-setup/assets/scripts/app.js
+++ working_with_DOM/dom-06-demo-project-setup/assets/scripts/app.js
@@ -3,3 +3,9 @@ const addMovieModal = document.getElementById("add-modal");
 //const addMovieModal = document.body.children[1];
 const startAddMovieBotton = document.querySelector("header button");
 //const startAddMovieBotton = document.querySelector("header").lastElementChild;
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+};
+
+startAddMovieBotton.addEventListener("click", toggleMovieModal);
