4fe7652623b1343338c9639208213c1e30d479ec
jarv <rodrigo.ulloa22@gmail.com>
2026-06-03 22:11:47 -0400

    lesson 179 starting with the confirmation logic

--- working_with_DOM/dom-06-demo-project-setup/assets/scripts/app.js
+++ working_with_DOM/dom-06-demo-project-setup/assets/scripts/app.js
@@ -41,7 +41,7 @@ const clearMovieInput = () => {
   }
 };
 
-const deleteMovie = (movieId) => {
+const deleteMovieHandler = (movieId) => {
   let movieIndex = 0;
   for (const movie of movies) {
     if (movie.id === movieId) {
@@ -60,10 +60,17 @@ const closeMovieDeletionModal = () => {
   deleteMovieModal.classList.remove("visible");
 };
 
-const deleteMovieHandler = (movieId) => {
+const startDeleteMovieHandler = (movieId) => {
   deleteMovieModal.classList.add("visible");
   toggleBackdrop();
-  //deleteMovie(movieId);
+  const cancelDeletionButton = deleteMovieModal.querySelector(".btn--passive");
+  const confirmDeletionButton = deleteMovieModal.querySelector(".btn--danger");
+
+  cancelDeletionButton.addEventListener("click", closeMovieDeletionModal);
+  confirmDeletionButton.addEventListener(
+    "click",
+    deleteMovieHandler.bind(null, movieId),
+  );
 };
 
 const renderNewMovieElement = (id, title, imageUrl, rating) => {
@@ -78,7 +85,10 @@ const renderNewMovieElement = (id, title, imageUrl, rating) => {
       <p>${rating}/5</p>
     </div>
   `;
-  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+  newMovieElement.addEventListener(
+    "click",
+    startDeleteMovieHandler.bind(null, id),
+  );
   const listRoot = document.getElementById("movie-list");
   listRoot.append(newMovieElement);
 };
