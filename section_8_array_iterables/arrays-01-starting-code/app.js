760a88d6a335ad917ef63871c41b3d30314fd8f9
jarv <rodrigo.ulloa22@gmail.com>
2026-06-04 13:17:07 -0400

    lesson 191 Retrieving indexes with indexOf() /& lastIndexOf()

--- section_8_array_iterables/arrays-01-starting-code/app.js
+++ section_8_array_iterables/arrays-01-starting-code/app.js
@@ -46,9 +46,13 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 //const storedResults = testResults.slice(2);
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 console.log(storedResults, testResults);
+console.log(testResults.lastIndexOf(1.5));
+
+const personalData = [{ name: "Max" }, { name: "Piri" }];
+console.log(personalData.indexOf({ name: "Piri" }));
