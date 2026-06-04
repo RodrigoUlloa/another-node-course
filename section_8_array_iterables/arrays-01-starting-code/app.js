229edf55367020f470087afb89d9ac6559c661c3
jarv <rodrigo.ulloa22@gmail.com>
2026-06-03 23:50:53 -0400

    lesson 185 arrys

---
+++ section_8_array_iterables/arrays-01-starting-code/app.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = Array(5);
+// console.log(moreNumbers);
+//
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+const listItem = document.querySelectorAll("li");
+console.log(listItem);
+
+const moreNumbers = Array.from(listItem);
+console.log(moreNumbers);
