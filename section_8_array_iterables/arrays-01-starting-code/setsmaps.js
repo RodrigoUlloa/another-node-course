908a8344761f0717288c1396e7a56673fc494fbf
jarv <rodrigo.ulloa22@gmail.com>
2026-06-05 00:08:43 -0400

    lesson 205 working with Sets

---
+++ section_8_array_iterables/arrays-01-starting-code/setsmaps.js
@@ -0,0 +1,9 @@
+// const ids = new Set(["Hi", "from", "set!"]);
+// ids.add(2);
+// if (ids.delete("Hi")) {
+//   ids.delete("Hi");
+// }
+//
+// for (const entry of ids.entries()) {
+//   console.log(entry[0]);
+// }
