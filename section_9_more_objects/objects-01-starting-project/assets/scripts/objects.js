839ba2732fe5d8e1440bc9e65076d66b7f0e6752
jarv <rodrigo.ulloa22@gmail.com>
2026-06-05 16:02:27 -0400

    lesson 218 property types & property order

--- section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
+++ section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
@@ -10,6 +10,7 @@ let person = {
   greet: function () {
     alert("Hi there");
   },
+  1.5: "hello",
 };
 
 //person.age = 31;
@@ -19,3 +20,5 @@ person.age = null;
 person.isAdmin = true;
 
 console.log(person["first name"]);
+console.log(person[1.5]);
+console.log(person);
