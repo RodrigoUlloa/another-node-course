262f58341830cda187cafe2a75cf34b64cf03c6f
jarv <rodrigo.ulloa22@gmail.com>
2026-06-05 16:40:21 -0400

    lesson 219 Dynamic property Access & Setting Properties

--- section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
+++ section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById("movie-list");
 movieList.style["backgroundColor"] = "red";
 movieList.style.display = "block";
 
+const userChosenKeyName = "level";
+
 let person = {
   "first name": "Piri",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  [userChosenKeyName]: "...",
   greet: function () {
     alert("Hi there");
   },
@@ -16,9 +19,11 @@ let person = {
 //person.age = 31;
 delete person.age;
 //person.age = undefined;
-person.age = null;
+//person.age = null;
 person.isAdmin = true;
 
-console.log(person["first name"]);
+const keyName = "first name";
+
+console.log(person[keyName]);
 console.log(person[1.5]);
 console.log(person);
