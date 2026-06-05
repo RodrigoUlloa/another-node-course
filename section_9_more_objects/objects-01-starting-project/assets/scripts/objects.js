513d55e6e16d1c659cdcef5499e239e5f9044d37
jarv <rodrigo.ulloa22@gmail.com>
2026-06-05 14:16:45 -0400

    lesson 217 special key & square brackets property

--- section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
+++ section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
@@ -1,5 +1,10 @@
+const movieList = document.getElementById("movie-list");
+
+movieList.style["backgroundColor"] = "red";
+movieList.style.display = "block";
+
 let person = {
-  name: "Piri",
+  "first name": "Piri",
   age: 30,
   hobbies: ["Sports", "Cooking"],
   greet: function () {
@@ -13,4 +18,4 @@ delete person.age;
 person.age = null;
 person.isAdmin = true;
 
-console.log(person);
+console.log(person["first name"]);
