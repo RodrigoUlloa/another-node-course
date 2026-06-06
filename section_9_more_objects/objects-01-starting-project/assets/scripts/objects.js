5a94b7b63a76e70f9d8012bfb0aaf622fd02bd9c
jarv <rodrigo.ulloa22@gmail.com>
2026-06-05 23:09:53 -0400

    lesso 232 call() and apply()

--- section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
+++ section_9_more_objects/objects-01-starting-project/assets/scripts/objects.js
@@ -25,8 +25,9 @@ const renderMovies = (filter = "") => {
     console.log(otherProps);
     //const { title: movieTitle } = info;
     let { getFormattedTitle } = movie;
-    getFormattedTitle = getFormattedTitle.bind(movie);
-    let text = getFormattedTitle() + " - ";
+    //getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.call(movie);
+    movie + " - ";
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
