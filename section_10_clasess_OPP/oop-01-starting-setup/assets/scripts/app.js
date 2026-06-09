81d8c8227e77bdc4588429225e5b576d74d92b74
jarv <rodrigo.ulloa22@gmail.com>
2026-06-08 23:36:39 -0400

    lesson 248 communicating can be challengin

--- section_10_clasess_OPP/oop-01-starting-setup/assets/scripts/app.js
+++ section_10_clasess_OPP/oop-01-starting-setup/assets/scripts/app.js
@@ -15,6 +15,11 @@ class Product {
 class ShoopingCart {
   items = [];
 
+  addItem(product) {
+    this.items.push(product);
+    this.totalOutput = `<h2>Total: \$${1}</h2>`;
+  }
+
   render() {
     const cartEl = document.createElement("section");
     cartEl.innerHTML = `
@@ -22,6 +27,7 @@ class ShoopingCart {
       <button>Order now!</button>
     `;
     cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -34,6 +40,7 @@ class ProductItem {
   addToCart() {
     console.log("Adding product to cart...");
     console.log(this.product);
+    ShoopingCart.addProduct();
   }
 
   render() {
