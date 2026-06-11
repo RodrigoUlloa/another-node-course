2eb7f381fc26dc3c40fc5cd75ab78f948a8e863b
jarv <rodrigo.ulloa22@gmail.com>
2026-06-10 23:42:55 -0400

    lesson 257 different ways of adding methods

--- section_10_clasess_OPP/oop-01-starting-setup/assets/scripts/app.js
+++ section_10_clasess_OPP/oop-01-starting-setup/assets/scripts/app.js
@@ -61,7 +61,12 @@ class ShoopingCart extends Component {
   }
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log("Ordering...");
+      console.log(this.items);
+    };
+    this.render();
   }
 
   addProduct(product) {
@@ -76,6 +81,9 @@ class ShoopingCart extends Component {
       <h2>Total: \$${0}</h2>
       <button>Order now!</button>
     `;
+    const orderButton = cartEl.querySelector("button");
+    //orderButton.addEventListener("click", () => this.orderProducts());
+    orderButton.addEventListener("click", this.orderProducts);
     this.totalOutput = cartEl.querySelector("h2");
   }
 }
