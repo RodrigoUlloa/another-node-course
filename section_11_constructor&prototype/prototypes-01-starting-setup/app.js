cef1af1e74e793b0bdf5a044f551ea2d53ebe6ee
jarv <rodrigo.ulloa22@gmail.com>
2026-06-11 23:58:13 -0400

    lesson 272 classes & prorotypes

--- section_11_constructor&prototype/prototypes-01-starting-setup/app.js
+++ section_11_constructor&prototype/prototypes-01-starting-setup/app.js
@@ -1,32 +1,36 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
-//
-// class Person extends AgedPerson {
-//   name = "Max";
-//
-//   constructor() {
-//     super();
-//     this.age = 30;
-//   }
-//
-//   greet() {
-//     console.log(
-//       "Hi, I am " + this.name + " and I am " + this.age + " years old.",
-//     );
-//   }
-// }
-function Person() {
-  this.age = 30;
-  this.name = "Max";
-  this.greet = function () {
+class AgedPerson {
+  constructor() {
+    console.log(this.name);
+  }
+
+  printAge() {
+    console.log(this.age);
+  }
+}
+
+class Person extends AgedPerson {
+  name = "Max";
+
+  constructor() {
+    super();
+    this.age = 30;
+  }
+
+  greet() {
     console.log(
       "Hi, I am " + this.name + " and I am " + this.age + " years old.",
     );
-  };
+  }
 }
+// function Person() {
+//   this.age = 30;
+//   this.name = "Max";
+//   this.greet = function () {
+//     console.log(
+//       "Hi, I am " + this.name + " and I am " + this.age + " years old.",
+//     );
+//   };
+// }
 
 // Person.prototype = {
 //   printAge() {
