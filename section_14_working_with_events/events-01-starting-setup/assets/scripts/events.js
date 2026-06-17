05be8d26b8365eb81ef07194317d075249f14538
jarv <rodrigo.ulloa22@gmail.com>
2026-06-17 12:22:22 -0400

    different ways of listening to events

---
+++ section_14_working_with_events/events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+// button.onclick = function() {
+//
+// };
+
+// const buttonClickHandler = () => {
+//   alert("Button was clicked!");
+// };
+//
+// const antoherButtonClickHandler = () => {
+//   console.log("This was clicked");
+// };
+//
+// button.onclick = buttonClickHandler;
+// button.onclick = antoherButtonClickHandler;
+
+button.addEventListener();
+
+//button.removeEventListener();
