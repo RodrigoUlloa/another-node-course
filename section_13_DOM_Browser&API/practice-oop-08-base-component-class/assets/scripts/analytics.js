3df143082e24887c4fbb12f8e8be0980fde9e318
jarv <rodrigo.ulloa22@gmail.com>
2026-06-16 23:34:25 -0400

    lesson 297 setting timers & intervals

--- section_13_DOM_Browser&API/practice-oop-08-base-component-class/assets/scripts/analytics.js
+++ section_13_DOM_Browser&API/practice-oop-08-base-component-class/assets/scripts/analytics.js
@@ -1,1 +1,11 @@
-console.log("Sending analitics");
+const intervalId = setInterval(
+  () => {
+    console.log("Sending analytics  data..");
+  },
+  2000,
+  [],
+);
+
+document.getElementById("stop-analytics-btn").addEventListener("click", () => {
+  clearInterval(intervalId);
+});
