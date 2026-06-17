cb55dacda8b43baa2355b7e64598dec093d61dbe
jarv <rodrigo.ulloa22@gmail.com>
2026-06-17 14:12:50 -0400

    lesson 308 the event object

--- section_14_working_with_events/events-01-starting-setup/assets/scripts/events.js
+++ section_14_working_with_events/events-01-starting-setup/assets/scripts/events.js
@@ -1,10 +1,11 @@
-const button = document.querySelector("button");
+const buttons = document.querySelectorAll("button");
 // button.onclick = function() {
 //
 // };
 
-const buttonClickHandler = () => {
-  alert("Button was clicked!");
+const buttonClickHandler = (event) => {
+  event.target.disabled = true;
+  console.log(event);
 };
 
 const antoherButtonClickHandler = () => {
@@ -16,8 +17,12 @@ const antoherButtonClickHandler = () => {
 
 const boundFn = buttonClickHandler.bind(this);
 
-button.addEventListener("click", buttonClickHandler.bind(this));
+//button.addEventListener("click", buttonClickHandler);
 
-setTimeout(() => {
-  button.removeEventListener("click", buttonClickHandler.bind(this));
-}, 2000);
+// setTimeout(() => {
+//   button.removeEventListener("click", buttonClickHandler);
+// }, 2000);
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", buttonClickHandler);
+});
