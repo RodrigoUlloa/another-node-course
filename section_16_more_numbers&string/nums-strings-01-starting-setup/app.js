38aeb9c0c4a14143e34f3654498eb7beff0ed6ce
jarv <rodrigo.ulloa22@gmail.com>
2026-06-24 17:00:00 -0400

    lesson 343 tagged templates

--- section_16_more_numbers&string/nums-strings-01-starting-setup/app.js
+++ section_16_more_numbers&string/nums-strings-01-starting-setup/app.js
@@ -3,3 +3,21 @@ function randomIntBetween(min, max) {
 }
 
 console.log(randomIntBetween(1, 10));
+
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let priceCategory = "pretty cheap regarding its price";
+  if (productPrice > 20) {
+    priceCategory = "fairly priced";
+  }
+  //return `${strings[0]}${productName}${strin[1]}${priceCategory}${strings[2]}`;
+  return { name: productName, price: productPrice };
+}
+
+const prodName = "Javscript course";
+const prodPrice = 29.99;
+
+const productOutPut = productDescription`This procut (${prodName}) i ${prodPrice}.`;
+console.log(productOutPut);
